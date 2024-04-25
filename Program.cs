5738929c4eec0c7cf71496150c21e20eb576d9cd
Afsar Nadheem C <afsar17052002@gmail.com>
2024-04-25 08:18:36 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ENUM
+{
+    class Program {
+
+        enum newEnum {
+            One = 1,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven
+            
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Direct Access : " + newEnum.One);
+
+            Console.WriteLine("Enum Value : " + (int)newEnum.One);
+
+            // Creating Instance;
+            newEnum newenum = newEnum.Five;
+
+            Console.WriteLine("Instance : " + newenum);
+
+            Console.WriteLine("Instance Enum Value : " + (int)newenum);
+
+
+        }
+    }
+}
